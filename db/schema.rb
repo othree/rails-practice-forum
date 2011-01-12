0d3c21c337df262ffd1c10afecb22e520168f1ce
othree <othree@gmail.com>
2011-01-12 15:18:36 +0800

    add user_id to :post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110112060814) do
+ActiveRecord::Schema.define(:version => 20110126060815) do
 
   create_table "forums", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110112060814) do
     t.integer  "forum_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
